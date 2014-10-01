610c93cc1885637f946b87be20f16cafe4b5314c
Alex Yang <movingmt@gmail.com>
2014-10-01 21:37:02 +0900

    create MoneyTest class. create Dollar class.

---
+++ tdd_java_kent_beck/tdd001/src/main/java/biz/btpeople/study/tdd/Dollar.java
@@ -0,0 +1,16 @@
+package biz.btpeople.study.tdd;
+
+public class Dollar {
+
+	public int amount;
+
+	public Dollar(int amount) {
+		// TODO Auto-generated constructor stub
+	}
+
+	public void times(int i) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
